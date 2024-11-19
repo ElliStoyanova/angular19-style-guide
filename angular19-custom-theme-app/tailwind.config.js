5341d12181104f2e2f29a920a68a2a542888da89
ElliStoyanova <elli.debelli@gmail.com>
2024-11-19 15:21:43 +0200

    Installing tailwind css

---
+++ angular19-custom-theme-app/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px'
+      },
+      extend: {
+          colors: {
+              active: '#004b99'
+          }
+      }
+  },
+  plugins: [],
+}
+
