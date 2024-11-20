b46886025e6be1b0db76703a7619a75aa9af98b9
ElliStoyanova <elli.debelli@gmail.com>
2024-11-20 10:57:08 +0200

    upgrade to angular 19

--- angular19-custom-theme-app/src/app/app.component.ts
+++ angular19-custom-theme-app/src/app/app.component.ts
@@ -4,14 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet, 
-    MatSlideToggleModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        MatSlideToggleModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular19-custom-theme-app';
