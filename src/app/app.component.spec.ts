36c9f9c26cbddcf8ed9504d5ee9658cc82adb063
ElliStoyanova <elli.debelli@gmail.com>
2024-11-24 14:18:04 +0200

    move project to repo root folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular19-custom-theme-app' title`, () => {
+  it(`should have the 'angular19-style-guide' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular19-custom-theme-app');
+    expect(app.title).toEqual('angular19-style-guide');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular19-custom-theme-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular19-style-guide');
   });
 });
